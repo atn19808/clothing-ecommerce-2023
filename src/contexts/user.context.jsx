35a4301f97129cd2362a415db4186eedd39cfc4e
unknown <atn1908@outlook.com>
2023-06-22 17:11:04 -0500

    more details about Observer Pattern

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,6 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.util";
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -10,6 +12,16 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser}
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if (user) {
+                createUserDocumentFromAuth(user);
+            }
+            setCurrentUser(user);
+        });
+        return unsubscribe;
+    }, []);
 
     return <UserContext.Provider value={value} >{ children }</UserContext.Provider>
 }
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,11 +8,10 @@ import { signOutUser } from "../../utils/firebase/firebase.util";
 import './navigation.styles.scss';
 
 const Navigation = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const { currentUser } = useContext(UserContext);
 
     const signOutHandler = async() => {
         const res = await signOutUser();
-        setCurrentUser(null);
     };
 
     return (

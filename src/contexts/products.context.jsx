a63518492e8dbb4581a26e092868844e9aaae92c
unknown <atn1908@outlook.com>
2023-06-22 17:44:10 -0500

    fix minor

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,11 +2,11 @@ import { useState, createContext } from "react";
 
 import PRODUCTS from '../shop-data.json';
 
-export const ProductContext = createContext({
+export const ProductsContext = createContext({
     products: [],
 });
 
-export const ProductProvider = ({children}) => {
+export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState(PRODUCTS);
     const value = {products};
     return (

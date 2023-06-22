f38f417919f8811c85c87151a0d889374c124393
unknown <atn1908@outlook.com>
2023-06-22 16:21:16 -0500

    add sign out feature

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-
+import { useState, useContext } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.util";
+import { UserContext } from "../../contexts/user.context";
+
 import FormInput from "../form-input/form-input.component";
 import "./sign-up-form.styles.scss";
 import Button from "../button/button.component";
@@ -16,6 +17,8 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const { setCurrentUser } = useContext(UserContext);
+
     console.log(formFields);
 
     const resetFormFields = () => {
@@ -30,6 +33,8 @@ const SignUpForm = () => {
         }
         try {
             const {user} = await createAuthUserWithEmailAndPassword(email, password);
+            setCurrentUser(user);
+
             await createUserDocumentFromAuth(user, {displayName});
             resetFormFields();
         } catch(error) {

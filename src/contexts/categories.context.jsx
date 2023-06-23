a7fedb81b533ede2d66b162b783cd18dc52b4f61
unknown <atn1908@outlook.com>
2023-06-22 23:29:34 -0500

    add categories map from firebase store

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,24 @@
+import { useState, createContext, useEffect } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.util";
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+
+    useEffect(() => {
+        const getCategoriesMap = async() => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        };
+        getCategoriesMap();
+    }, []);
+
+    const value = {categoriesMap};
+    return (
+        <CategoriesProvider.Provider value={value}>{children}</CategoriesProvider.Provider>
+    )
+}

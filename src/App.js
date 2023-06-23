fafec10f14e2b33f9e59c0351b2d6967a57bc63c
unknown <atn1908@outlook.com>
2023-06-23 03:53:43 -0500

    fix not loading sign up page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.component'
-import Navigation  from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
@@ -9,14 +9,14 @@ import Checkout from './routes/checkout/checkout.component';
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />} >
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
-        <Route path='authentication' element={<Authentication />} />
-      </Route> 
+      </Route>
     </Routes>
   );
-}
+};
 
 export default App;
